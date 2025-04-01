3d1e1b62ba65811c6f70a98c21b79797c1320d30
mgdtk <marlon.destro@hotmail.com>
2025-04-01 19:39:33 -0300

    Adicionar função conversor()

--- Aula 01/script.js
+++ Aula 01/script.js
@@ -1,30 +1,32 @@
-let moeda = prompt("Qual moeda deseja converter? Digite Won, Euro ou Dolar.");
+function conversor() { 
+    let moeda = prompt("Qual moeda deseja converter? Digite Won, Euro ou Dolar.");
 
-while (moeda.toLowerCase() !== "won" && moeda.toLowerCase() !== "euro" && moeda.toLowerCase() !== "dolar") {
-    moeda = prompt("Entrada inválida. Qual moeda deseja converter? Digite Won, Euro ou Dolar.");
-}
+    while (moeda.toLowerCase() !== "won" && moeda.toLowerCase() !== "euro" && moeda.toLowerCase() !== "dolar") {
+        moeda = prompt("Entrada inválida. Qual moeda deseja converter? Digite Won, Euro ou Dolar.");
+    }
 
-let valor = parseFloat(prompt("Qual valor (em reais) deseja converter?"));
+    let valor = parseFloat(prompt("Qual valor (em reais) deseja converter?"));
 
-while (isNaN(valor) || valor === "" || valor <= 0) {
-    valor = parseFloat(prompt("Entrada inválida. Qual valor (em reais) deseja converter?"));
-}
+    while (isNaN(valor) || valor === "" || valor <= 0) {
+        valor = parseFloat(prompt("Entrada inválida. Qual valor (em reais) deseja converter?"));
+    }
 
-let cambio = {
-    won: 257.026, // 1000 BRL = 257026 KRW
-    dolar: 0.1744, // 1 BRL = 0.1744 USD
-    euro: 0.1615 // 1 BRL = 0.1615 EUR
-};
+    let cambio = {
+        won: 257.026, // 1000 BRL = 257026 KRW
+        dolar: 0.1744, // 1 BRL = 0.1744 USD
+        euro: 0.1615 // 1 BRL = 0.1615 EUR
+    };
 
-moeda = moeda.toLowerCase();
-valorFormatado = valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
+    moeda = moeda.toLowerCase();
+    valorFormatado = valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
 
-if (cambio[moeda]) {
-    let valorConvertido = (cambio[moeda] * valor).toLocaleString("pt-BR", {style: "decimal"});
-    console.log(valorConvertido);
+    if (cambio[moeda]) {
+        let valorConvertido = (cambio[moeda] * valor).toLocaleString("pt-BR", {style: "decimal"});
+        console.log(valorConvertido);
 
-    let nomeMoeda = moeda === "dolar" ? "Dólares" : `${moeda.charAt(0).toUpperCase() + moeda.slice(1)}s`;
-    alert(`${valorFormatado} equivale a ${valorConvertido} ${nomeMoeda}.`);
-} else {
-    alert("Erro.");
+        let nomeMoeda = moeda === "dolar" ? "Dólares" : `${moeda.charAt(0).toUpperCase() + moeda.slice(1)}s`;
+        alert(`${valorFormatado} equivale a ${valorConvertido} ${nomeMoeda}.`);
+    } else {
+        alert("Erro.");
+    }
 }
