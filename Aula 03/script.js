62982acf5d4e43aab513b6c207cfb3f12a4715ed
mgdtk <marlon.destro@hotmail.com>
2025-04-02 14:02:45 -0300

    Fazer o desafio proposto em Aula

--- Aula 03/script.js
+++ Aula 03/script.js
@@ -1,21 +1,33 @@
-rodada = 1;
+function jogar() {
+    let rodada = 1;
+    let vitoria = 0;
 
-while (rodada <= 3) {
-    console.log(`Rodada: ${rodada}`);
+    while (rodada <= 3) {
+        console.log(`Rodada: ${rodada}`);
 
-    escolhaJogador = prompt(`Rodada ${rodada}. Escolha o vidro 1, 2 ou 3.`);
-    pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+        let pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+        let escolhaJogador = parseInt(prompt(`Rodada ${rodada}. Escolha o vidro 1, 2 ou 3.`));
+        
+        while (isNaN(escolhaJogador) || escolhaJogador < 1 || escolhaJogador > 3) {
+            escolhaJogador = parseInt(prompt(`Entrada inválida. Digite 1, 2 ou 3.`));
+        }
 
-    if (escolhaJogador == pisoQuebrado) {
-        alert("O vidro quebrou! Você perdeu.");
-        rodada = 1000;
-    } else {
-        alert(`Passou! O piso quebrado era o vidro ${pisoQuebrado}`);
+        if (escolhaJogador == pisoQuebrado) {
+            alert("O vidro quebrou! Você perdeu.");
+            break;
+        } else {
+            alert(`Passou! O piso quebrado era o vidro ${pisoQuebrado}.`);
+            if (rodada == 3) {
+                vitoria = 1;
+            }
+        }
+
+        rodada += 1;
     }
 
-    rodada = rodada + 1;
-}
+    console.log(vitoria);
 
-if (rodada == 4) {
-    alert("Parabéns! Você venceu!");
+    if (vitoria) {
+        alert("Parabéns! Você venceu!");
+    }
 }
