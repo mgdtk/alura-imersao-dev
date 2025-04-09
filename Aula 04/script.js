65bb124f3aa9620266dba1b1bd5e560d5383b742
mgdtk <marlon.destro@hotmail.com>
2025-04-08 23:05:33 -0300

    Centralizar elementos e adaptar manipulação do DOM para usar classes
    Pequenos ajustes nas estilizações de alguns elementos

--- Aula 04/script.js
+++ Aula 04/script.js
@@ -1,41 +1,42 @@
 let forcaTotalJogador = 0;
 let forcaTotalComputador = 0;
 
-const personagens = {
-    "Harry Potter": 5,
-    "Lord Voldemort": 5,
-    "Albus Dumbledore": 5,
-    "Hermione Granger": 4,
-    "Severus Snape": 4,
-    "Draco Malfoy": 3,
-    "Sirius Black": 4,
-    "Minerva McGonagall": 4,
-    "Rubeus Hagrid": 3,
-    "Ginny Weasley": 3,
-    "Neville Longbottom": 3,
-    "Luna Lovegood": 2,
-    "Fred Weasley": 2,
-    "George Weasley": 2,
-    "Remus Lupin": 4,
-    "Bellatrix Lestrange": 4,
-    "Lucius Malfoy": 3,
-    "Dolores Umbridge": 2,
-    "Molly Weasley": 3,
-    "Arthur Weasley": 2,
-    "Cedric Diggory": 3,
-    "Cho Chang": 2,
-    "Kingsley Shacklebolt": 4,
-    "Nymphadora Tonks": 3,
-    "Gilderoy Lockhart": 1,
-    "Filius Flitwick": 3,
-    "Horace Slughorn": 3,
-    "Peter Pettigrew": 1,
-    "Victor Krum": 3
-};
+// const personagens = {
+//     "Harry Potter": 5,
+//     "Lord Voldemort": 5,
+//     "Albus Dumbledore": 5,
+//     "Hermione Granger": 4,
+//     "Severus Snape": 4,
+//     "Draco Malfoy": 3,
+//     "Sirius Black": 4,
+//     "Minerva McGonagall": 4,
+//     "Rubeus Hagrid": 3,
+//     "Ginny Weasley": 3,
+//     "Neville Longbottom": 3,
+//     "Luna Lovegood": 2,
+//     "Fred Weasley": 2,
+//     "George Weasley": 2,
+//     "Remus Lupin": 4,
+//     "Bellatrix Lestrange": 4,
+//     "Lucius Malfoy": 3,
+//     "Dolores Umbridge": 2,
+//     "Molly Weasley": 3,
+//     "Arthur Weasley": 2,
+//     "Cedric Diggory": 3,
+//     "Cho Chang": 2,
+//     "Kingsley Shacklebolt": 4,
+//     "Nymphadora Tonks": 3,
+//     "Gilderoy Lockhart": 1,
+//     "Filius Flitwick": 3,
+//     "Horace Slughorn": 3,
+//     "Peter Pettigrew": 1,
+//     "Victor Krum": 3
+// };
 
 function escolher() {
     document.querySelector(".telaInicio").style.display = "none";
     document.querySelector(".selecionarPersonagens").style.display = "flex";
+    document.querySelector(".inputsPersonagens").style.display = "flex";
 }
 
 function jogar() {
@@ -73,4 +74,5 @@ function verResultado() {
 
     document.getElementById("informacoesJogador").innerHTML = `A força total do seu time foi: ${forcaTotalJogador}`;
     document.getElementById("informacoesComputador").innerHTML = `A força total do time do computador foi: ${forcaTotalComputador}`;
+    document.querySelector(".informacoesResultados").style.display = "flex";
 }
