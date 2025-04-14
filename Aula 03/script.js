fa23899326dc9ea06ce43ebbf16070be577e7ae0
mgdtk <marlon.destro@hotmail.com>
2025-04-14 12:02:04 -0300

    Começar manipulação do DOM para interações do usuário

--- Aula 03/script.js
+++ Aula 03/script.js
@@ -1,54 +1,68 @@
-function jogar() {
-    let rodadas = prompt("Quantas rodadas deseja jogar?");
-    if (verificarCancelamento(rodadas)) return;
+function escolherRodadas() {
+    document.querySelector(".telaInicial").style.display = "none";
+    document.querySelector(".telaRodadas").style.display = "flex";
+}
+
+function validarRodadas() {
+    let rodadas = document.getElementById("numRodadas").value;
     rodadas = parseInt(rodadas);
 
-    while (isNaN(rodadas) || rodadas < 1) {
-        rodadas = prompt("Entrada inválida. Digite o número de rodadas que deseja jogar.");
-        if (verificarCancelamento(rodadas)) return;
-        rodadas = parseInt(rodadas);
+    if (isNaN(rodadas) || rodadas <= 0) {
+        document.getElementById("numRodadasInvalido").style.display = "flex";
+    } else {
+        document.getElementById("numRodadasInvalido").style.display = "none";
+        escolherTabuada();
     }
+}
 
-    let tabuada = prompt("Deseja ser desafiado até a tabuada de qual número?");
-    if (verificarCancelamento(tabuada)) return;
+function escolherTabuada() {
+    document.querySelector(".telaRodadas").style.display = "none";
+    document.querySelector(".telaTabuada").style.display = "flex";
+}
+
+function validarTabuada() {
+    let tabuada = document.getElementById("numTabuada").value;
     tabuada = parseInt(tabuada);
 
-    while (isNaN(tabuada) || tabuada < 0) {
-        tabuada = prompt("Entrada inválida. Digite o número da tabuada limite que gostaria de jogar.");
-        if (verificarCancelamento(tabuada)) return;
-        tabuada = parseInt(tabuada);
-    }
-
-    for (let i = 1; i < (rodadas + 1); i++) {
-        let primeiroNumero = Math.floor(Math.random() * (tabuada + 1));
-        let segundoNumero = Math.floor(Math.random() * (tabuada + 1));
-
-        let resultado = primeiroNumero * segundoNumero;
-
-        let palpiteJogador = prompt(`Rodada ${i} de ${rodadas}. Digite o resultado da seguinte multiplicação: ${primeiroNumero}x${segundoNumero}`);
-        if (verificarCancelamento(palpiteJogador)) return;
-
-        while (isNaN(palpiteJogador) || palpiteJogador == "") {
-            palpiteJogador = prompt(`Entrada inválida. Digite um número para o resultado da seguinte multiplicação: ${primeiroNumero}x${segundoNumero}`);
-            if (verificarCancelamento(palpiteJogador)) return;
-        }
-
-        if (palpiteJogador != resultado) {
-            alert(`Você errou! O resultado era ${resultado}.`);
-            return;
-        } else {
-            alert(`Acertou!`);
-            if (i == rodadas) {
-                alert("Parabéns! Você venceu!");
-            }
-        }
+    if (isNaN(tabuada) || tabuada < 0) {
+        document.getElementById("tabuadaInvalida").style.display = "flex";
+    } else {
+        document.getElementById("tabuadaInvalida").style.display = "none";
+        jogar();
     }
 }
 
-function verificarCancelamento(valor) {
-    if (valor === null) {
-        console.log("Operação cancelada.");
-        return true;
-    }
-    return false;
-}
+// function jogar() {
+//     for (let i = 1; i < (rodadas + 1); i++) {
+//         let primeiroNumero = Math.floor(Math.random() * (tabuada + 1));
+//         let segundoNumero = Math.floor(Math.random() * (tabuada + 1));
+
+//         let resultado = primeiroNumero * segundoNumero;
+
+//         let palpiteJogador = prompt(`Rodada ${i} de ${rodadas}. Digite o resultado da seguinte multiplicação: ${primeiroNumero}x${segundoNumero}`);
+//         if (verificarCancelamento(palpiteJogador)) return;
+
+//         while (isNaN(palpiteJogador) || palpiteJogador == "") {
+//             palpiteJogador = prompt(`Entrada inválida. Digite um número para o resultado da seguinte multiplicação: ${primeiroNumero}x${segundoNumero}`);
+//             if (verificarCancelamento(palpiteJogador)) return;
+//         }
+
+//         if (palpiteJogador != resultado) {
+//             alert(`Você errou! O resultado era ${resultado}.`);
+//             return;
+//         } else {
+//             alert(`Acertou!`);
+//             if (i == rodadas) {
+//                 alert("Parabéns! Você venceu!");
+//             }
+//         }
+//     }
+// }
+
+// function verificarCancelamento(valor) {
+//     if (valor === null) {
+//         console.log("Operação cancelada.");
+//         return true;
+//     }
+//     return false;
+// }
